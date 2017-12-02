daa2a4144381d8a6ce67acb14ad9beb5c731120f
袁庆龙 <yuanql96>
2017-12-02 23:14:09 +0800

    完成90%照片墙功能

--- js/index.js
+++ js/index.js
@@ -35,13 +35,23 @@ require(["particles"],function(){
 //照片墙
  require(['jquery','showWork'],function($,ShowWork){
         var settings1 = {
-            selector : "#portfolio .row",                            //要求加上‘#’、‘.’等选择器标识符。
-            selection :['All','Brand','Design','Photo','Video'],
+            selector : "#portfolio .row",                                     //要求加上‘#’、‘.’等选择器标识符。
+            selection :['All','Brand','Design','Photo','Video'],              //类名，第一个必须是All
+            itemName:["个人网站","旋转阴阳鱼","3","4","5","6"],                    //项目名
+            itemClass:['Brand','Design','Photo','Video','Brand','Design'],      //图片分类
+            itemAddress:[
+                'https://github.com/JokerDr/persenal-web',
+                'https://github.com/JokerDr/css3-',
+                '',
+                '',
+                '',
+                ''],
+            imgSrcAndName:['img/showImg','s','jpg'],     //如s1.jpg,s2.jpg,只需要传入s,另外尽量传入和itemCount相同数量的图片
             speed : 1000,
-            imgSrcAndName:['img/showImg','s','jpg'],     //如s1.jpg,s2.jpg,只需要传入s,另外尽量传入和itemCount相同数量的图片，
+            fontAwesomeName:'fa fa-search',
             selectCount:5,
             itemCount:6
-        }
+        };
         var ShowWork1 = new ShowWork(settings1);
         ShowWork1.init();
  });
--- js/showWork.js
+++ js/showWork.js
@@ -7,17 +7,22 @@ requirejs.config({
             carousel:'carousel'
         }
 });
-//考虑到es6兼容性问题，并没有采用模板字符串，有时间再做QAQ，尽量做成单独的组件
+//没有采用模板字符串，有时间再做QAQ，尽量做成单独的组件
 define(["jquery"],function($){
     function ShowWork(settings){
         this.$filtering = $('<div class="filtering"></div>');
         this.$gallery = $('<div class="gallery"></div>');
         this.$img = $('<img alt="image">');
         this.$h4 = $('<h4></h4>');
+        this.$a =$('<a><i></i></a>')
         this.defaultSettings = {
             selector : "",
-            selection :['All','Brand','Design','Photo','Video'],
+            selection :[],
+            itemName:[],
+            itemClass:[],
+            itemAddress:[],
             imgSrcAndName:[],
+            fontAwesomeName:'',
             speed : 1000,
             selectCount:4,
             itemCount:6
@@ -26,31 +31,70 @@ define(["jquery"],function($){
     }
     ShowWork.prototype.init=function(){
         $(this.defaultSettings.selector).append(this.$filtering);
-
         //动态创建分类数量
         for(var i=0;i<this.defaultSettings.selectCount;i++){
             var index = i;
             $('.filtering').append("<span>" + this.defaultSettings.selection[index] + "</span>");
-        };
-        $(this.defaultSettings.selector).append(this.$gallery);
 
+        }
+        $('.filtering').children('span').on('click',function () {
+           // if($('.filtering').children('span'))
+           //  console.log($('.filtering').children('span'));
+            $('.filtering').children('span').filter(function (index) {
+                // console.log(index)
+            })
+        });
+        $(this.defaultSettings.selector).append(this.$gallery);
         //动态创建展示的div的数量
         for(var i = 0;i<this.defaultSettings.itemCount; i++){
             $('.gallery').append('<div class="itemImg"></div>' );
         }
         $('.itemImg').append(this.$img);
-
-
-        //向img标签内加入图片
-        for(var i = 0;i<=this.defaultSettings.itemCount;i++){
-            // var nowindex = i;
+        $('.itemImg').append(this.$h4).append(this.$a);
+        $('.itemImg').find('i').addClass(this.defaultSettings.fontAwesomeName);
+        //向<img>里添加图片,向h4里添加名字，添加链接
+        for(var i = 0;i<this.defaultSettings.itemCount;i++){
+            addClass( $('.gallery').children()[i], this.defaultSettings.itemClass[i]);
+            $('.itemImg').find('a')[i].href = this.defaultSettings.itemAddress[i];
+            $('.itemImg').find('h4')[i].innerText = this.defaultSettings.itemName[i];
             $('.itemImg').find('img')[i].src = this.defaultSettings.imgSrcAndName[0] + "/" + this.defaultSettings.imgSrcAndName[1] + i + "." + this.defaultSettings.imgSrcAndName[2];
+            add (i);
         }
-
-        $('.itemImg').find('img').onmouseover(function(){
-
-        })
-
+        //封装一个addclass函数
+        function addClass(obj, cls){
+            var obj_class = obj.className;  //获取 class 内容.
+            var blank = (obj_class != '') ? ' ' : '';//判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
+            var added = obj_class + blank + cls;//组合原来的 class 和需要添加的 class.
+            obj.className = added;//替换原来的 class.
+        }
+        function add (index) {
+            if(window.addEventListener){
+                $('.itemImg')[index].addEventListener('mouseover',function(){
+                    $(this).addClass('imgmask');
+                    gotVal('scale(1.4)','0','rotateX(0deg)','3','50%',index);
+                });
+                $('.itemImg')[index].addEventListener('mouseout',function(){
+                    $(this).removeClass('imgmask');
+                    gotVal('scale(1)','-20%','rotateX(90deg)','-1','0',index);
+                });
+            }else{
+                $('.itemImg')[index].attachEvent('onmouseover',function () {
+                    $(this).addClass('imgmask');
+                    gotVal('scale(1.4)','0','rotateX(0deg)','3','50%',index);
+                });
+                $('.itemImg')[index].attachEvent('onmouseover',function(){
+                    $(this).removeClass('imgmask');
+                    gotVal('scale(1)','-20%','rotateX(90deg)','-1','0',index);
+                });
+            }
+         }
+         function gotVal(a,b,c,d,e,index){
+             $('.itemImg').find('img')[index].style.transform = a;
+             $('.itemImg').find('h4')[index].style.left = b;
+             $('.itemImg').find('h4')[index].style.transform = c;
+             $('.itemImg').find('a')[index].style.zIndex = d;
+             $('.itemImg').find('a')[index].style.bottom = e;
+         }
     }
     return ShowWork;
-})
+});
